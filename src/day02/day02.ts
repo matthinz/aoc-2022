5998731134fecf1ec5a08833a285ceed93ebfc92
Matt Hinz <matthinz@gmail.com>
2022-12-02 13:37:05 -0800

    Day 2

---
+++ src/day02/day02.ts
@@ -0,0 +1,101 @@
+import { getInputLines } from "../utils";
+
+run().catch((err) => {
+  process.exitCode = 1;
+  console.error(err);
+});
+
+async function run() {
+  const input = await getInputLines();
+  console.log(partOne(input));
+  console.log(partTwo(input));
+}
+
+const ROCK = { strength: 12 };
+
+type Player = "rock" | "paper" | "scissors";
+
+type Rule = {
+  id: Player;
+  aka: string[];
+  value: number;
+  beats: Player;
+};
+const rules: Rule[] = [
+  {
+    id: "rock",
+    aka: ["A", "X"],
+    value: 1,
+    beats: "scissors",
+  },
+  {
+    id: "paper",
+    aka: ["B", "Y"],
+    value: 2,
+    beats: "rock",
+  },
+  {
+    id: "scissors",
+    aka: ["C", "Z"],
+    value: 3,
+    beats: "paper",
+  },
+];
+
+function partOne(input: string[]): number {
+  // What would your total score be if everything goes exactly according to your strategy guide?
+  return input.reduce((totalScore, round) => {
+    const [theirMove, myMove] = round.split(" ");
+    const me = rules.find((r) => r.aka.includes(myMove));
+    const them = rules.find((r) => r.aka.includes(theirMove));
+
+    if (!me || !them) {
+      throw new Error();
+    }
+
+    return totalScore + scoreRound(me, them);
+  }, 0);
+}
+
+function partTwo(input: string[]): number {
+  // what would your total score be if everything goes exactly according to your strategy guide?
+
+  return input.reduce(function (totalScore, round) {
+    const [theirMove, desiredOutcome] = round.split(" ");
+    const them = rules.find((r) => r.aka.includes(theirMove));
+
+    if (!them) {
+      throw new Error();
+    }
+
+    let me: Rule | undefined;
+
+    if (desiredOutcome === "X") {
+      // i need to lose, find a move that they will beat
+      me = rules.find((r) => r.id === them.beats);
+    } else if (desiredOutcome === "Y") {
+      // i need to draw, use their same move
+      me = them;
+    } else if (desiredOutcome === "Z") {
+      // i need to win, find a move that beats them
+      me = rules.find((r) => r.beats === them.id);
+    }
+
+    if (!me) {
+      throw new Error();
+    }
+
+    return totalScore + scoreRound(me, them);
+  }, 0);
+}
+
+function scoreRound(me: Rule, them: Rule): number {
+  let score = me.value;
+
+  if (me === them) {
+    score += 3;
+  } else if (me.beats == them.id) {
+    score += 6;
+  }
+  return score;
+}
