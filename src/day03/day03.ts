7a2febd1db01e1e56a517c0a21678189485255cd
Matt Hinz <matthinz@gmail.com>
2022-12-04 01:33:19 +0000

    Day 3 part 2
    done from a phone!!

--- src/day03/day03.ts
+++ src/day03/day03.ts
@@ -25,24 +25,47 @@ function partOne(input: string[]): number {
       throw new Error();
     }
 
-    // lowercase priority = 1-26
-    // uppercase = 27 - 52
-    const c = item.charCodeAt(0);
-    let priority: number;
-
-    if (item.toUpperCase() === item) {
-      // uppercase ascii A=65
-      priority = c - 65 + 27;
-    } else if (item.toLowerCase() === item) {
-      priority = c - 97 + 1;
-    } else throw new Error();
-
-    return total + priority;
-
-    return 0;
+    return total + calcPriority(item);
   }, 0);
 }
 
 function partTwo(input: string[]): number {
-  return 0;
+  // go through input in batches of 3
+  // find item type common to them
+  // sum the priorities
+
+  const BATCH_SIZE = 3;
+
+  return input.reduce(function (total, line, index) {
+    if (index % BATCH_SIZE !== 0) {
+      return total;
+    }
+
+    const othersInGroup = input.slice(index + 1, index + BATCH_SIZE);
+    const item = line.split("").find((c) => {
+      return othersInGroup.every((o) => o.indexOf(c) >= 0);
+    });
+
+    if (!item) {
+      throw new Error();
+    }
+
+    return total + calcPriority(item);
+  }, 0);
+}
+
+function calcPriority(item: string): number {
+  // lowercase priority = 1-26
+  // uppercase = 27 - 52
+  const c = item.charCodeAt(0);
+  let priority: number;
+
+  if (item.toUpperCase() === item) {
+    // uppercase ascii A=65
+    return c - 65 + 27;
+  } else if (item.toLowerCase() === item) {
+    return c - 97 + 1;
+  } else {
+    throw new Error();
+  }
 }
