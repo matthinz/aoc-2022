80d485cd219ab116f4f5d1c3b93e6d0681a8f7c9
Matt Hinz <matthinz@gmail.com>
2022-12-05 17:04:14 +0000

    Day 5 part 1

---
+++ src/day05/day05.ts
@@ -0,0 +1,93 @@
+import { getInputLines } from "../utils";
+
+type Move = {
+  from: number;
+  to: number;
+  count: number;
+};
+
+type Input = {
+  stacks: string[][];
+  moves: Move[];
+};
+
+run().catch((err) => {
+  process.exitCode = 1;
+  console.error(err);
+});
+
+async function run() {
+  const input = await getInputLines();
+  console.log(partOne(input));
+  console.log(partTwo(input));
+}
+
+function partOne(input: string[]): string {
+  const parsed = parseInput(input);
+
+  // apply moves
+
+  parsed.moves.forEach((m) => {
+    for (let i = 0; i < m.count; i++) {
+      const value = parsed.stacks[m.from - 1].pop();
+
+      if (!value) {
+        throw new Error();
+      }
+      parsed.stacks[m.to - 1].push(value);
+    }
+  });
+
+  return parsed.stacks.map((s) => s[s.length - 1] ?? "").join("");
+}
+
+function partTwo(input: string[]): string {
+  return "";
+}
+
+function parseInput(input: string[]): Input {
+  const stacks: string[][] = [];
+  const moves: Move[] = [];
+
+  let inStacks = true;
+  input.forEach((line) => {
+    if (/^\s*$/.test(line)) {
+      return;
+    }
+
+    if (/^(\s*\d+)+\s*$/.test(line)) {
+      inStacks = false;
+      return;
+    }
+
+    const m = /move (\d+) from (\d+) to (\d+)/.exec(line);
+
+    if (m) {
+      inStacks = false;
+      moves.push({
+        from: parseInt(m[2], 10),
+        to: parseInt(m[3], 10),
+        count: parseInt(m[1], 10),
+      });
+    }
+
+    if (!inStacks) {
+      return;
+    }
+
+    // put in empty slots to ease parsing
+    line = line.replace(/ {4}(?= )/g, " [ ]");
+
+    line = line.replace(/(^\[|\]$)/g, "");
+
+    line.split("] [").forEach((value, index) => {
+      if (value === " ") {
+        return;
+      }
+
+      stacks[index] = stacks[index] ?? [];
+      stacks[index].unshift(value);
+    });
+  });
+  return { stacks, moves };
+}
