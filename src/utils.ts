a23c140459a0bb8b83a4c296d527690cb06035ae
Matt Hinz <matthinz@gmail.com>
2022-12-08 15:15:52 -0800

    Read input file if nothing piped to stdin

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import { readLines } from "https://deno.land/std@0.167.0/io/mod.ts";
+import * as path from "https://deno.land/std@0.167.0/path/mod.ts";
 
 export async function getInputLines(
   input?: string,
@@ -13,9 +14,20 @@ export async function getInputLines(
     return input.split("\n");
   }
 
+  let reader: Reader = Deno.stdin;
+
+  if (Deno.isatty(Deno.stdin.rid)) {
+    // Nothing being piped in, try to load the input file for the main module
+    const inputFile = path.join(
+      path.dirname(path.fromFileUrl(Deno.mainModule)),
+      "input",
+    );
+    reader = await Deno.open(inputFile, { read: true });
+  }
+
   const allLines: string[] = [];
 
-  for await (const line of readLines(Deno.stdin)) {
+  for await (const line of readLines(reader)) {
     allLines.push(line);
   }
 
