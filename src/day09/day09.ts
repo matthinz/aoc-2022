a18553e1ab9acfc9c4b989a74f179fddb3f0a555
Matt Hinz <matthinz@gmail.com>
2022-12-08 22:06:48 -0800

    Day 9: Refactor to help w/ part 2

--- src/day09/day09.ts
+++ src/day09/day09.ts
@@ -42,7 +42,6 @@ export function partTwo(input: string[]): number | string {
 
 function move(head: Pos, tail: Pos, dir: Direction): [Pos, Pos] {
   const newHead = { ...head };
-  const newTail = { ...tail };
 
   switch (dir) {
     case "U":
@@ -59,65 +58,45 @@ function move(head: Pos, tail: Pos, dir: Direction): [Pos, Pos] {
       break;
   }
 
-  // Now we need to solve where the tail must be
+  const newTail = simulateMovement(newHead, tail);
 
-  // If the tail is w/in 1 in both the x + y direction, it doesn't need to move
-  const xDistance = newHead.x - newTail.x;
-  const yDistance = newHead.y - newTail.y;
+  return [newHead, newTail];
+}
+
+// given two positions, returns the new position for <b>
+function simulateMovement(a: Pos, b: Pos): Pos {
+  const xDistance = a.x - b.x;
+  const yDistance = a.y - b.y;
 
   if (Math.abs(xDistance) <= 1 && Math.abs(yDistance) <= 1) {
-    return [newHead, newTail];
+    return b;
   }
+
+  const result = { ...b };
 
   // When head and tail share an axis, it's a little easier
   if (xDistance === 0) {
     // Positive yDistance means head is above tail
-    newTail.y += yDistance + (yDistance > 0 ? -1 : 1);
+    result.y += yDistance + (yDistance > 0 ? -1 : 1);
   } else if (yDistance === 0) {
     // Positive xDistance means head is to the right of tail
-    newTail.x += xDistance + (xDistance > 0 ? -1 : 1);
+    result.x += xDistance + (xDistance > 0 ? -1 : 1);
   } else if (Math.abs(yDistance) > Math.abs(xDistance)) {
     // When head and tail don't share an axis, tail will snap
     // to the axis that it's closest away on
     // we're farthest away on the y axis, so tail will snap to same x coord
-    newTail.x = newHead.x;
-    newTail.y += yDistance + (yDistance > 0 ? -1 : 1);
+    result.x = a.x;
+    result.y += yDistance + (yDistance > 0 ? -1 : 1);
   } else if (Math.abs(yDistance) < Math.abs(xDistance)) {
     // We're farthest away on x axis, so we snap to same y
-    newTail.y = newHead.y;
-    newTail.x += xDistance + (xDistance > 0 ? -1 : 1);
+    result.y = a.y;
+    result.x += xDistance + (xDistance > 0 ? -1 : 1);
   } else {
     console.error({ xDistance, yDistance });
     throw new Error("didn't handle this");
   }
 
-  return [newHead, newTail];
-
-  // H
-
-  // H
-  // T
-
-  // TH
-
-  // HT
-
-  // T
-  // H
-
-  // T
-  //  H
-
-  //  T
-  // H
-
-  //  H
-  // T
-
-  // H
-  //  T
-
-  return [head, tail];
+  return result;
 }
 
 if (import.meta.main) {
