d4bbb92a94ddbec7114bb1cdc481224eee34eb89
Matt Hinz <matthinz@gmail.com>
2022-12-28 21:17:17 -0800

    Day 20, part 1
    Use linked list for great justice

--- src/day20/day20.ts
+++ src/day20/day20.ts
@@ -1,17 +1,33 @@
 import { runDay } from "../aoc.ts";
 
-export function partOne(input: string[]): number | string {
-  const numbers = input.map((line) => parseInt(line, 10));
-  mix(numbers);
+type Node = {
+  value: number;
+  originalPosition: number;
+  prev: Node;
+  next: Node;
+};
 
-  const zeroPos = numbers.indexOf(0);
-  if (zeroPos < 0) {
-    throw new Error("No 0 in the list");
+export function partOne(input: string[]): number | string {
+  const { node, values } = parseInput(input);
+  mix(node, values, console.error.bind(console));
+
+  let zeroNode: Node;
+  for (let n = node; n; n = n.next) {
+    if (n.value === 0) {
+      zeroNode = n;
+      break;
+    }
   }
 
   return [1000, 2000, 3000].reduce(
     function (result, offset) {
-      return result + numbers[(zeroPos + offset) % numbers.length];
+      // find the node <offset> after the zero node
+      offset = offset % values.length;
+      let n = zeroNode;
+      for (let i = 0; i < offset; i++) {
+        n = n.next;
+      }
+      return result + n.value;
     },
     0,
   );
@@ -21,75 +37,86 @@ export function partTwo(input: string[]): number | string {
   return "";
 }
 
-function mix(numbers: number[], log?: (...args: any[]) => void) {
-  const l = log ?? (() => {});
+function mix(rootNode: Node, values: number[], log?: (...args: any[]) => void) {
+  values.forEach((value, pos) => {
+    if (value === 0) {
+      return;
+    }
 
-  // transforms is an array of functions used to translate a position from
-  // the old array to the new one.
-  const transforms: ((pos: number) => number)[] = [];
+    const node = findNodeWithOriginalPosition(pos);
 
-  l("Initial arrangement:");
-  l(numbers.join(", "));
+    // remove node from its old position
+    const oldPrev = node.prev;
+    const oldNext = node.next;
+    oldPrev.next = oldNext;
+    oldNext.prev = oldPrev;
 
-  [...numbers].forEach((num, pos, ar) => {
-    const oldPos = transforms.reduce((result, tx) => tx(result), pos);
-    let newPos: number;
+    let newNext: Node;
+    let newPrev: Node;
 
-    const delta = num % ar.length;
-
-    if (delta > 0) {
-      if (oldPos + delta === ar.length - 1) {
-        // When moving forward, if we hit the last position, we're actually
-        // moving to the beginning of the list
-        newPos = 0;
-      } else if (oldPos + delta >= ar.length) {
-        newPos = (oldPos + delta + 1) % ar.length;
-      } else {
-        newPos = oldPos + delta;
-      }
-    } else if (delta < 0) {
-      if (oldPos + delta === 0) {
-        // When moving backwards, if we hit position 0, we're actually
-        // at the _end_ of the list
-        newPos = ar.length - 1;
-      } else if (oldPos + delta < 0) {
-        newPos = ar.length + (oldPos + delta - 1);
-      } else {
-        newPos = oldPos + delta;
-      }
+    if (value < 0) {
+      // we're going to move backward and insert _before_ the node at that place
+      // [ 1, 2, -2]
+      newNext = offset(node, value);
+      newPrev = newNext.prev;
     } else {
-      newPos = oldPos;
+      // we're going to move forward and insert _after_ the node at that place
+      newPrev = offset(node, value);
+      newNext = newPrev.next;
     }
 
-    if (newPos > oldPos) {
-      numbers.splice(newPos + 1, 0, num);
-      numbers.splice(oldPos, 1);
-    } else if (newPos < oldPos) {
-      numbers.splice(newPos, 0, num);
-      numbers.splice(oldPos + 1, 1);
-    }
-    transforms.push((p) => {
-      if (p === oldPos) {
-        return newPos;
-      } else if (p > oldPos && p <= newPos) {
-        return p - 1;
-      } else {
-        return p;
-      }
-    });
-
-    if (newPos === oldPos) {
-      l("\n%d does not move:", num);
-    } else {
-      l(
-        "\n%d moves between %d and %d:",
-        num,
-        newPos === 0 ? numbers[numbers.length - 1] : numbers[newPos - 1],
-        newPos === numbers.length - 1 ? numbers[0] : numbers[newPos + 1],
-      );
-    }
-    l(numbers.join(", "));
+    node.prev = newPrev;
+    node.next = newNext;
+    newNext.prev = node;
+    newPrev.next = node;
   });
+
+  function findNodeWithOriginalPosition(pos: number): Node {
+    for (let n = rootNode; n; n = n.next) {
+      if (n.originalPosition === pos) {
+        return n;
+      }
+    }
+    throw new Error();
+  }
+
+  function offset(node: Node, distance: number): Node {
+    for (let i = 0; i < Math.abs(distance); i++) {
+      if (distance > 0) {
+        node = node.next;
+      } else {
+        node = node.prev;
+      }
+    }
+
+    return node;
+  }
+}
+
+function parseInput(input: string[]): { node: Node; values: number[] } {
+  type InProgressNode = {
+    value: number;
+    originalPosition: number;
+    prev?: InProgressNode;
+    next?: InProgressNode;
+  };
+
+  const values = input.map((line) => parseInt(line, 10));
+
+  const nodes: InProgressNode[] = values.map((value, originalPosition) => ({
+    value,
+    originalPosition,
+  }));
+
+  for (let i = 0; i < nodes.length; i++) {
+    nodes[i].prev = nodes[i - 1];
+    nodes[i].next = nodes[i + 1];
+  }
+
+  nodes[0].prev = nodes[nodes.length - 1];
+  nodes[nodes.length - 1].next = nodes[0];
+
+  return { node: nodes[0] as Node, values };
 }
 
 if (import.meta.main) {
